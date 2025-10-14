392f9dd87c8e7b5280575a05758ba3df7f78fb5f
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-10-14 22:39:12 +0200

    add literate, update totorial

--- docs/src/12-tutorial.jl
+++ docs/src/12-tutorial.jl
@@ -13,10 +13,12 @@ theme(:boxed)
 # ## Basic Breit-Wigner Functions
 
 # Simple constant-width Breit-Wigner
-bw1 = BreitWigner(1.5, 0.1);  # mass=1.5 GeV, width=0.1 GeV
+# and an energy-dependent Breit-Wigner with form factors are created as follows
 
-# Energy-dependent Breit-Wigner with form factors
+bw1 = BreitWigner(1.5, 0.1);  # mass=1.5 GeV, width=0.1 GeV
 bw2 = BreitWigner(1.5, 0.1, 0.5, 0.3, 1, 1.0);  # ma=0.5, mb=0.3, l=1, d=1.0
+
+# They shapes are similar since the channel threshold at 0.1+0.5 = 0.6 GeV is far away from the nominal mass of 1.5 GeV.
 
 let
     m_range = range((bw1.m .+ bw1.Γ .* [-5, 3])..., 100)
@@ -25,9 +27,9 @@ let
     plot!(m_range, m -> bw2(m^2) |> abs2, label = "Energy dependent", lw = 2)
 end
 
+# Let's take a more complex example with two channels with different masses and couplings.
 
-# Two-channel Breit-Wigner
-bw3 = MultichannelBreitWigner(
+bw3 = MultichannelBreitWigner(  # Two-channel Breit-Wigner
     1.5,
     [
         (gsq = 0.18, ma = 0.1, mb = 0.5, l = 1, d = 1.5),
@@ -35,12 +37,13 @@ bw3 = MultichannelBreitWigner(
     ],
 );
 
+# The coupling of the first channel is adjusted to have a similar width as `bw1`, the second channel in S-wave is introduced to show a prominent distortion of the shape.
+
 let
     m_range = range((bw1.m .+ bw1.Γ .* [-5, 3])..., 200)
     plot(xlab = "m (GeV)", ylab = "|BW|²")
     plot!(m_range, m -> bw2(m^2) |> abs2, label = "Energy dependent", lw = 2)
     plot!(m_range, m -> bw3(m^2) |> abs2, label = "Two-channel BW", lw = 2)
-    #
     vspan!(bw3.channels[2].ma .+ [-1, 1] .* bw3.channels[2].mb, lc = :gray, alpha = 0.1)
     vline!(bw3.channels[2].ma .+ [-1, 1] .* bw3.channels[2].mb, lc = :gray)
     annotate!(bw3.channels[2].ma, 20, text("channel-2 momentum\nis complex", :center, 10))
